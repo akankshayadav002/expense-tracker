856a84addaf8d315f2b3dcfaf6c4cac1712d1317
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2024-03-14 18:44:11 +0530

    db connection

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,51 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
 
 import mergedResolvers from "./resolver/index.resolver.js"
 import mergedTypeDefs from "./typeDefs/index.typeDef.js"
+import { connectDB } from "./db/connectDB.js";
+dotenv.config()
 
+const app = express();
+
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs:mergedTypeDefs,
   resolvers:mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+
+
 })
 
-const { url } = await startStandaloneServer(server)
 
-console.log(`🚀 Server ready at ${url}`)
+await server.start();
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+	"/",
+	cors(),
+	express.json(),
+	// expressMiddleware accepts the same arguments:
+	// an Apollo Server instance and optional configuration options
+	expressMiddleware(server, {
+		context: async ({ req}) => ({ req }),
+	})
+);
+
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
